3c1d7f74ffb6764709d6e1d188e7c6e66647913d
penre <Tianyou.zhang25@ycdsbk12.ca>
2024-04-15 17:07:43 -0400

    finished to level 3 idk level 4

--- Sketch.java
+++ Sketch.java
@@ -22,23 +22,66 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {  
-    drawBullseye(200,200);
-    drawBullseye(400,200);
-    drawBullseye(600,200);
+    //draw first row
+    drawBullseye(150,150);
+    drawBullseye(400,150);
+    drawBullseye(650,150);
+    //second row
+    drawBullseye(150,400);
+    drawBullseye(400,400);
+    drawBullseye(650,400);
+    //third row
+    drawBullseye(150,650);
+    drawBullseye(400,650);
+    drawBullseye(650,650);
+    //draw spongebob first row
+    drawSpongebob(240,240); 
+    drawSpongebob(490,240);
+    //draw spongebob second row
+    drawSpongebob(240,470); 
+    drawSpongebob(490,470);
+
     
 
    
     
 }
-private void drawBullseye(float horseX, float horseY){
-  fill(250,0,0);
-  ellipse(horseX,horseY,200, 200); 
-  fill(255);
-  ellipse(horseX, horseY, 150,  150);
-  fill(0);
-  ellipse(horseX, horseY, 100, 100);
-  fill(255);
-  ellipse(horseX,horseY, 50,50);
+  private void drawBullseye(float bullX, float bullY){
+    //draw the bullseye
+    fill(250,0,0);
+    ellipse(bullX,bullY,200, 200); 
+    fill(255);
+    ellipse(bullX, bullY, 150,  150);
+    fill(0);
+    ellipse(bullX, bullY, 100, 100);
+    fill(255);
+    ellipse(bullX,bullY, 50,50);
+    fill(0);
+    ellipse(bullX,bullY, 25,25);
+    fill(250,0,0);
+    ellipse(bullX,bullY, 10,10);
   
 }
+  private void drawSpongebob(float spongeX, float spongeY){
+    //draw spongebob
+    fill(250,250,0);
+    rect(spongeX,spongeY,70,90);
+    //draw spongebob face
+    fill(255);
+    ellipse(spongeX + 20,spongeY + 20, 30, 30);
+    ellipse(spongeX + 50,spongeY + 20, 30, 30);
+    fill(0);
+    ellipse(spongeX + 20,spongeY + 20, 10, 10);
+    ellipse(spongeX + 50,spongeY + 20, 10, 10);
+    //draw spongebob mouth
+    line(spongeX + 20,spongeY + 50, spongeX + 40, spongeY + 50);
+    //draw spongebob teeth
+    fill(255);
+    rect(spongeX + 20,spongeY + 50, 10, 10);
+    rect(spongeX + 40,spongeY + 50, 10, 10);
+    //draw spongebob pants
+    fill(165,42,42);
+    rect(spongeX, spongeY + 70, 70, 20);
+
+  }
 }
